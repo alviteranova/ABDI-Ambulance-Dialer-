f6463ea07536199d348eabd18d7ea0f6c1af65d8
Alvi <alviteranovasuryana@gmail.com>
2018-08-29 13:20:07 +0700

    Tambah Gambar

--- app/src/main/java/com/kelompok2/abdiambulancedialer/SecondActivity.java
+++ app/src/main/java/com/kelompok2/abdiambulancedialer/SecondActivity.java
@@ -1,7 +1,12 @@
 package com.kelompok2.abdiambulancedialer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        final EditText nama = (EditText)findViewById(R.id.nama);
+        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
+        final TextView txtHasil = (TextView)findViewById(R.id.txtHasil);
+        Button btnPindah = (Button)findViewById(R.id.btnPindah);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ambil;
+                ambil = nama.getText().toString();
+
+                txtHasil.setText(ambil);
+            }
+        });
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent pindahkemain = new Intent(pakageCon)
+            }
+        });
     }
 }
